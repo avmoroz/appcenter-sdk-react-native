97535fc6bead2fb0621e04733d9c456033b8a0bd
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-06-12 17:01:18 -0700

    Add initial Demo App for Windows

---
+++ DemoAppWin/windows/DemoAppWin/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::DemoAppWin::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::DemoAppWin::implementation
+
+
