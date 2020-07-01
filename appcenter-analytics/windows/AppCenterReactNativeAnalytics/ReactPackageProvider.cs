69acfcb4447e2fec143710462423f2a48b18694a
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-07-01 16:33:03 -0700

    Modify appcenter-analytics. Add windows support to appcenter-analytics.

---
+++ appcenter-analytics/windows/AppCenterReactNativeAnalytics/ReactPackageProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+
+namespace AppCenterReactNativeAnalytics
+{
+    public sealed class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            packageBuilder.AddAttributedModules();
+            packageBuilder.AddViewManagers();
+        }
+    }
+}
