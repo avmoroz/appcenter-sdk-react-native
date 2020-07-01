788ecb607bc0d91fc4d37cc3385bc8e689ddbb8d
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-07-01 16:38:01 -0700

    Add DemoAppWinCS2

---
+++ DemoAppWinCS2/windows/Microsoft.ReactNative.AppCenter/ReactPackageProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ReactNative.Managed;
+
+namespace Microsoft.ReactNative.AppCenter
+{
+    public sealed class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            packageBuilder.AddAttributedModules();
+            packageBuilder.AddViewManagers();
+        }
+    }
+}
